5a0b2692f70c3f84226799fcf2f23676477488eb
Tomas Gregor <tomas.gregor@me.com>
2012-11-12 12:32:45 -0600

    TG: changes in User's _form and model file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name, :password_digest, :surname
+  attr_accessible :email, :name, :password, :password_confirmation, :surname
+  
+  has_secure_password
+  
 end
