342639dfd01f2cc827a507f197262eb8c9e75010
Tomas Gregor <tomas.gregor@me.com>
2012-11-20 12:25:51 -0600

    TG: added user_id to foods table

--- app/models/food.rb
+++ app/models/food.rb
@@ -1,6 +1,10 @@
 class Food < ActiveRecord::Base
   attr_accessible :category_id, :name, :picture, :remote_picture_url, :price, :posts_attributes
+  
   has_many :posts
+  
   accepts_nested_attributes_for :posts
+  
   mount_uploader :picture, PictureUploader
+  
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121112182035) do
+ActiveRecord::Schema.define(:version => 20121120182450) do
 
   create_table "foods", :force => true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(:version => 20121112182035) do
     t.integer  "category_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.integer  "user_id"
   end
 
   create_table "posts", :force => true do |t|
