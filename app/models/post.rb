8117cb96e5c6156b314e3b5534f50732c29e8cb6
Tomas Gregor <tomas.gregor@me.com>
2012-11-26 19:00:53 -0600

    TG: minor change in Post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ActiveRecord::Base
-  attr_accessible :note, :picture, :rating, :food_id 
+  attr_accessible :note, :picture, :rating, :user_id, :postable_id, :postable_type 
   
   belongs_to :user
   belongs_to :postable, :polymorphic => true
